3ab15a6eee70833961b716cdd023db0eb39e6f0c
shivksh <49638585+shivksh@users.noreply.github.com>
2020-08-21 01:00:48 +0530

    Add files via upload

---
+++ SendWelcomeEmailListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegisteredEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeEmail;
+use App\Jobs\SendEmailJob;
+
+
+class SendWelcomeEmailListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserRegisteredEvent  $event
+     * @return void
+     */
+    public function handle(UserRegisteredEvent $event)
+    {
+        
+        // Mail::to($event->user)->send(new WelcomeEmail());
+        // dispatch(new SendEmailJob())
+        SendEmailJob::dispatch()
+        ->delay(now()->addSeconds(5));
+
+
+    }
+}
